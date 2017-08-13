5554b9b1d9a3671f25a76316d884c33ee4f1feeb
abhijeet <abhijeet@tymescripts.com>
2017-08-13 19:18:55 +0530

    1)Since settings option 'wpsa_settings' is an multidimension aray hence assertEquals replace with assertContains 2)Added Test page multi select option is saved

--- tests/test-wp-solwininfotech-assignment-2a.php
+++ tests/test-wp-solwininfotech-assignment-2a.php
@@ -6,7 +6,7 @@
  */
 
 /**
- * Sample test case.
+ * Test case for Assignment-2a: Simple Alert Plugin.
  */
 class Wp_Solwininfotech_Assignment_2a_Test extends WP_UnitTestCase {
 
@@ -70,8 +70,8 @@ class Wp_Solwininfotech_Assignment_2a_Test extends WP_UnitTestCase {
 	public function test_alert_text_option_is_saved() {
 		// Simulate $_POST variable.
 		$_POST['wpsa_alert_text_field'] = $this->wpsa_alert_text_field;
-		update_option( 'wpsa_alert_text_field', $_POST['wpsa_alert_text_field'] );
-		$this->assertEquals( $this->wpsa_alert_text_field, get_option( 'wpsa_alert_text_field' ) );
+		update_option( 'wpsa_settings', array( $_POST['wpsa_alert_text_field'] ) );
+		$this->assertContains( $this->wpsa_alert_text_field, get_option( 'wpsa_settings' ) );
 	}
 
 	/**
@@ -79,17 +79,17 @@ class Wp_Solwininfotech_Assignment_2a_Test extends WP_UnitTestCase {
 	 */
 	public function test_check_box_is_saved() {
 		// Simulate $_POST check box variable (checked value is 1).
-		$_POST['wpsa_alert_text_field_1'] = 1;
-		update_option( 'wpsa_alert_text_field_1', $_POST['wpsa_alert_text_field_1'] );
-		$this->assertEquals( 1, get_option( 'wpsa_alert_text_field_1' ) );
+		$_POST['wpsa_checkbox_field_1'] = 1;
+		update_option( 'wpsa_settings', array( $_POST['wpsa_checkbox_field_1'] ) );
+		$this->assertContains( 1, get_option( 'wpsa_settings' ) );
 	}
 
 	/**
-	 * Test multi select option is saved
+	 * Test posts multi select option is saved
 	 */
 	public function test_posts_multi_select_option_is_saved() {
 
-		// Create temproray posts.
+		// Create temproray posts post_type.
 		for ( $i = 0; $i < 5; $i++ ) {
 				$this->factory()->post->create(
 					array(
@@ -103,6 +103,7 @@ class Wp_Solwininfotech_Assignment_2a_Test extends WP_UnitTestCase {
 		// Check if cout returned is 5.
 		$count_posts = wp_count_posts( 'post', 'readable' );
 		$this->assertEquals( 5, $count_posts->publish );
+
 		$args = array(
 			'post_type'   => 'post',
 			'post_status' => 'publish',
@@ -111,9 +112,43 @@ class Wp_Solwininfotech_Assignment_2a_Test extends WP_UnitTestCase {
 		$posts_types_array = $posts_types_array->posts;
 
 		// Simulate $_POST variable.
-		$_POST['wpsa_alert_text_field'] = $posts_types_array;
+		$_POST[ $this->multiselect_name ] = $posts_types_array;
 
-		update_option( $this->multiselect_name, $_POST['wpsa_alert_text_field'] );
-		$this->assertEquals( $posts_types_array, get_option( $this->multiselect_name ) );
+		update_option( 'wpsa_settings', array( $_POST[ $this->multiselect_name ] ) );
+		$this->assertContains( $posts_types_array, get_option( 'wpsa_settings' ) );
+	}
+
+	/**
+	 * Test page multi select option is saved
+	 */
+	public function test_page_multi_select_option_is_saved() {
+
+		// Create temproray page post_type.
+		for ( $i = 0; $i < 5; $i++ ) {
+				$this->factory()->post->create(
+					array(
+						'post_status' => 'publish',
+						'post_type' => 'page',
+						'post_title' => 'Page Title ' . $i,
+					)
+				);
+		}
+
+		// Check if count returned is 5.
+		$count_posts = wp_count_posts( 'page', 'readable' );
+		$this->assertEquals( 5, $count_posts->publish );
+
+		$args = array(
+			'post_type'   => 'page',
+			'post_status' => 'publish',
+		);
+		$page_post_type_array = new \WP_Query( $args );
+		$page_post_type_array = $page_post_type_array->posts;
+
+		// Simulate $_POST variable.
+		$_POST[ $this->multiselect_name ] = $page_post_type_array;
+
+		update_option( 'wpsa_settings', array( $_POST[ $this->multiselect_name ] ) );
+		$this->assertContains( $page_post_type_array, get_option( 'wpsa_settings' ) );
 	}
 }
