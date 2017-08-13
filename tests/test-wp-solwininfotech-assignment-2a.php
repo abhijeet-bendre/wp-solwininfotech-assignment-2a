bd0a7f31ededfdc47cfeb4d80f4f097ceaecd479
abhijeet <abhijeet@tymescripts.com>
2017-08-13 13:32:12 +0530

    Test multi select option is saved

--- tests/test-wp-solwininfotech-assignment-2a.php
+++ tests/test-wp-solwininfotech-assignment-2a.php
@@ -18,6 +18,13 @@ class Wp_Solwininfotech_Assignment_2a_Test extends WP_UnitTestCase {
 	private $wpsa_alert_text_field = 'Assignment-2a: Simple Alert Plugin';
 
 	/**
+	 * Multi Select option Key
+	 *
+	 * @var string private
+	 */
+	private $multiselect_name = 'wpsa_multi_select_field_1';
+
+	/**
 	 * Test if Plugin is active.
 	 */
 	function test_is_plugin_active() {
@@ -77,6 +84,36 @@ class Wp_Solwininfotech_Assignment_2a_Test extends WP_UnitTestCase {
 		$this->assertEquals( 1, get_option( 'wpsa_alert_text_field_1' ) );
 	}
 
+	/**
+	 * Test multi select option is saved
+	 */
+	public function test_posts_multi_select_option_is_saved() {
 
+		// Create temproray posts.
+		for ( $i = 0; $i < 5; $i++ ) {
+				$this->factory()->post->create(
+					array(
+						'post_status' => 'publish',
+						'post_type' => 'post',
+						'post_title' => 'Post Title ' . $i,
+					)
+				);
+		}
 
+		// Check if cout returned is 5.
+		$count_posts = wp_count_posts( 'post', 'readable' );
+		$this->assertEquals( 5, $count_posts->publish );
+		$args = array(
+			'post_type'   => 'post',
+			'post_status' => 'publish',
+		);
+		$posts_types_array = new \WP_Query( $args );
+		$posts_types_array = $posts_types_array->posts;
+
+		// Simulate $_POST variable.
+		$_POST['wpsa_alert_text_field'] = $posts_types_array;
+
+		update_option( $this->multiselect_name, $_POST['wpsa_alert_text_field'] );
+		$this->assertEquals( $posts_types_array, get_option( $this->multiselect_name ) );
+	}
 }
