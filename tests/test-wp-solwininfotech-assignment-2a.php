81b3918e95a54ae5e98c62a4c68e8417c7090e77
abhijeet <abhijeet@tymescripts.com>
2017-08-13 13:25:21 +0530

    Test if altleast one post and page exists

--- tests/test-wp-solwininfotech-assignment-2a.php
+++ tests/test-wp-solwininfotech-assignment-2a.php
@@ -17,4 +17,37 @@ class Wp_Solwininfotech_Assignment_2a_Test extends WP_UnitTestCase {
 		$this->assertTrue( is_plugin_active( WPSA_2A_PLUGIN_NAME . '/' . WPSA_2A_PLUGIN_NAME . 'php' ) );
 	}
 
+	/**
+	 * Test if altleast one post exists
+	 */
+	function test_if_altleast_one_post_type_post_exists() {
+		$this->factory()->post->create_many(
+			1,
+			array(
+				'post_status' => 'publish',
+				'post_type' => 'post',
+			)
+		);
+
+		$count1 = wp_count_posts( 'post', 'readable' );
+		$this->assertGreaterThanOrEqual( 1, $count1->publish );
+	}
+
+	/**
+	 * Test if altleast one page exists
+	 */
+	function test_if_altleast_one_post_type_page_exists() {
+		$this->factory()->post->create_many(
+			1,
+			array(
+				'post_status' => 'publish',
+				'post_type' => 'page',
+			)
+		);
+
+		$count1 = wp_count_posts( 'page', 'readable' );
+		$this->assertGreaterThanOrEqual( 1, $count1->publish );
+	}
+
+
 }
